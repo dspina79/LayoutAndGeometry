3e41dbda5fcc68f1bbb8cd87512ae603b4ddf3d8
Dave Spina <dspina79@gmail.com>
2021-02-17 14:22:42 -0500

    Project initalization

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Dave Spina on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
