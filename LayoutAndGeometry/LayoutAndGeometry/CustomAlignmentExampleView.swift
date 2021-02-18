97fda97e11385e5b053c22d5569e2908a6ac6b57
Dave Spina <dspina79@gmail.com>
2021-02-17 20:55:22 -0500

    Implemented custom alignment guide

---
+++ LayoutAndGeometry/LayoutAndGeometry/CustomAlignmentExampleView.swift
@@ -0,0 +1,55 @@
+//
+//  CustomAlignmentExampleView.swift
+//  LayoutAndGeometry
+//
+//  Created by Dave Spina on 2/17/21.
+//
+
+import SwiftUI
+
+extension VerticalAlignment {
+    // use enum to clearly identify that this is just used for name and
+    // limited functionality
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[VerticalAlignment.center]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct CustomAlignmentExampleView: View {
+    var body: some View {
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@davespina")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[VerticalAlignment.center]
+                    }
+                Image("project18")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 64)
+            }
+            VStack {
+                Text("Status")
+                Text("Active")
+                Text("Full Name")
+                    .bold()
+                    .alignmentGuide(.midAccountAndName) { d in
+                    d[VerticalAlignment.center]
+                }
+                Text("Dave Spina")
+                    .font(.largeTitle)
+            }
+            Text("Hello!!")
+        }
+    }
+}
+
+struct CustomAlignmentExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomAlignmentExampleView()
+    }
+}
